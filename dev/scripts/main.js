4922df82c49895fd3e22e29ec29eab3c0e27b869
Caterina van Herpt <caterina.vanherpt@gmail.com>
2017-09-19 01:06:34 -0400

    form and footer styling, adding responsive styles

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,26 @@
-const ohHey = "Hello World";
+$(function() {
 
-console.log(ohHey);
+	let submitted = false;
+
+	$('form').on('submit', () => {
+		submitted = true;
+	});
+
+	$('iframe').on('load', () => {
+		if (submitted) { 
+			window.location='index.html';
+		}
+	});
+
+	$('textarea').on('keyup', () => {
+		const textarea = document.getElementsByClassName('textarea');
+		textarea[0].style.height = (textarea[0].scrollHeight)+"px";
+	});
+
+	// $('input, textarea').on('click', () => {
+		
+	// });
+
+});
 
 
-console.log("hello")
